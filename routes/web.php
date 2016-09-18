2ce94aa91a8fe182969176dfff4c6202ef68c05d
Cliff Williams <cliff@hiddenpause.com>
2016-09-18 18:19:20 +0100

    Removed redundant route group

--- routes/web.php
+++ routes/web.php
@@ -7,24 +7,23 @@
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    Auth::routes();
 
-    // Homepage route
-    Route::get('/', 'HomeController@index');
+Auth::routes();
 
-    // City routes
-    Route::get('cities', 'CitiesController@index');
-    Route::get('cities/{city}', 'CitiesController@show');
-    Route::post('cities/{city}/places', 'PlacesController@store');
+// Homepage route
+Route::get('/', 'HomeController@index');
 
-    // Type routes
-    Route::get('types', 'TypesController@index');
-    Route::get('types/{type}', 'TypesController@show');
+// City routes
+Route::get('cities', 'CitiesController@index');
+Route::get('cities/{city}', 'CitiesController@show');
+Route::post('cities/{city}/places', 'PlacesController@store');
 
-    // Place routes
-    Route::get('places', 'PlacesController@index');
-    Route::get('places/{place}', 'PlacesController@show');
-    Route::get('places/{place}/edit', 'PlacesController@edit');
-    Route::patch('places/{place}/update', 'PlacesController@update');
-});
+// Type routes
+Route::get('types', 'TypesController@index');
+Route::get('types/{type}', 'TypesController@show');
+
+// Place routes
+Route::get('places', 'PlacesController@index');
+Route::get('places/{place}', 'PlacesController@show');
+Route::get('places/{place}/edit', 'PlacesController@edit');
+Route::patch('places/{place}/update', 'PlacesController@update');
