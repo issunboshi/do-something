5c8a8f3f28b0872b794601ad5823d722f9438993
Cliff Williams <cliff@hiddenpause.com>
2016-09-19 19:24:06 +0100

    Added skeleton of users testing

---
+++ tests/UsersTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UsersTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testNonAdminUser()
+    {
+        $user = factory(App\User::class)->make();
+    }
+
+    public function testAdminUser()
+    {
+        $user = factory(App\User::class, 'admin')->make();
+    }
+
+
+}
